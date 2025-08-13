e4f38c84d6cc51e352457d9005a929a9676bd440
victor botelho silva <victor_botelho_silva@hotmail.com>
2025-08-13 11:00:01 -0300

    feat: adicionei as funções no ClienteRepositoryImpl.java

--- src/main/java/one/digitalinnovation/gof/service/impl/ClienteServiceImpl.java
+++ src/main/java/one/digitalinnovation/gof/service/impl/ClienteServiceImpl.java
@@ -1,10 +1,16 @@
 package one.digitalinnovation.gof.service.impl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import one.digitalinnovation.gof.model.Cliente;
+import one.digitalinnovation.gof.model.ClienteRepository;
+import one.digitalinnovation.gof.model.Endereco;
+import one.digitalinnovation.gof.model.EnderecoRepository;
 import one.digitalinnovation.gof.service.ClienteService;
+import one.digitalinnovation.gof.service.ViaCepService;
 
 /**
  * Implementação da <b>Strategy</b> {@link ClienteService}, a qual pode ser
@@ -12,44 +18,68 @@ import one.digitalinnovation.gof.service.ClienteService;
  * {@link Service}, ela será tratada como um <b>Singleton</b>.
  * 
  * @author falvojr
+ * @author veketi
  */
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
 	// TODO Singleton: Injetar os componentes do Spring com @Autowired.
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private ViaCepService viaCepService;
 	// TODO Strategy: Implementar os métodos definidos na interface.
 	// TODO Facade: Abstrair integrações com subsistemas, provendo uma interface simples.
 
 	@Override
 	public Iterable<Cliente> buscarTodos() {
-		// FIXME Buscar todos os Clientes.
-		return null;
+		return clienteRepository.findAll();
 	}
 
 	@Override
 	public Cliente buscarPorId(Long id) {
-		// FIXME Buscar Cliente por ID.
-		return null;
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.get();
 	}
 
 	@Override
 	public void inserir(Cliente cliente) {
-		// FIXME Verificar se o Endereco do Cliente já existe (pelo CEP).
-		// FIXME Caso não exista, integrar com o ViaCEP e persistir o retorno.
-		// FIXME Inserir Cliente, vinculando o Endereco (novo ou existente).
+		salvarClienteComCep(cliente);
 	}
 
 	@Override
 	public void atualizar(Long id, Cliente cliente) {
 		// FIXME Buscar Cliente por ID, caso exista:
+		Optional<Cliente> clienteBd = clienteRepository.findById(id);
+		if(clienteBd.isPresent() == false) {
+			return;
+		}
 		// FIXME Verificar se o Endereco do Cliente já existe (pelo CEP).
 		// FIXME Caso não exista, integrar com o ViaCEP e persistir o retorno.
 		// FIXME Alterar Cliente, vinculando o Endereco (novo ou existente).
+		salvarClienteComCep(cliente);
+	}
+
+	private void salvarClienteComCep(Cliente cliente) {
+		// FIXME Verificar se o Endereco do Cliente já existe (pelo CEP).
+		String cep = cliente.getEndereco().getCep();
+		Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+			// FIXME Caso não exista, integrar com o ViaCEP e persistir o retorno.
+			Endereco novoEndereco = viaCepService.consultarCep(cep);
+			enderecoRepository.save(novoEndereco);
+			return novoEndereco;
+		});
+		cliente.setEndereco(endereco);
+		// FIXME Inserir Cliente, vinculando o Endereco (novo ou existente).
+		clienteRepository.save(cliente);
 	}
 
 	@Override
 	public void deletar(Long id) {
 		// FIXME Deletar Cliente por ID.
+		clienteRepository.deleteById(id);
 	}
 
 }
